ca695f9ced5d7916b6cc4c86109f4d109ba00df8
Rohan Jain <crodjer@gmail.com>
2017-11-19 21:34:43 +0530

    Generation of unique key
     - Key characters picked from RFC 4648 from Base32 alphabet.
     - Retries in case of conflicts.
     - CrateLink uses owned String's instead of str references.

--- src/handlers.rs
+++ src/handlers.rs
@@ -1,30 +1,40 @@
 extern crate diesel;
-
-use super::models;
-use super::schema;
+extern crate url;
 
 pub mod links {
     use diesel;
     use diesel::prelude::*;
     use models::links::{CreateLink,Link};
     use schema::links::table;
+    use super::url::{Url};
 
-    pub fn create_link<'a>(conn: &PgConnection, url: &'a str,
-                           key: Option<&'a str>, title: Option<&'a str>)
-        -> Link {
 
+    pub fn insert_link<'a>(conn: &PgConnection, link: &mut CreateLink, try: u8) -> Result<Link, Box<::std::error::Error>> {
+        match diesel::insert(link).into(table).get_result(conn) {
+            Ok(link) => Ok(link),
+            Err(e) => {
+                if link.is_custom || try > 2 {
+                    Err(Box::new(e))
+                } else {
+                    link.generate_key();
+                    insert_link(conn, link, try + 1)
+                }
+            }
+        }
+    }
+
+    pub fn create_link<'a>(conn: &PgConnection, url: &'a str, key: Option<&'a str>, title: Option<&'a str>) -> Result<Link, Box<::std::error::Error>> {
+        let url = Url::parse(url)?;
         let mut link = CreateLink::new(url);
+
         if let Some(key) = key {
             // The user wants to use a custom key.
-            link.customize(key);
+            link.customize(key.to_owned());
         }
         if let Some(title) = title {
             // The link also has a title!.
-            link.set_title(title);
+            link.set_title(title.to_owned());
         }
-
-        diesel::insert(&link).into(table)
-            .get_result(conn)
-            .expect("Error inserting link")
+        insert_link(conn, &mut link, 0)
     }
 }
--- src/models.rs
+++ src/models.rs
@@ -1,7 +1,10 @@
-use super::schema;
+extern crate rand;
+extern crate url;
 
 pub mod links {
     use schema::links;
+    use super::rand::{thread_rng, Rng};
+    use super::url::{Url};
 
     #[derive(Queryable)]
     #[derive(Debug)]
@@ -18,34 +21,55 @@ pub mod links {
     #[derive(Insertable)]
     #[derive(Debug)]
     #[table_name="links"]
-    pub struct CreateLink<'a> {
-        pub key: &'a str,
-        pub url: &'a str,
-        pub domain: &'a str,
-        pub title: &'a str,
+    pub struct CreateLink {
+        pub key: String,
+        pub url: String,
+        pub domain: String,
+        pub title: String,
         pub is_custom: bool
     }
 
-    impl <'a> CreateLink <'a> {
-        pub fn new(url: &'a str) -> CreateLink {
+    // Valid characters to be used for key generation.
+    // https://en.wikipedia.org/wiki/Base32#RFC_4648_Base32_alphabet
+    static VALID_CHARS: [char; 32] = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+                                      'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
+                                      'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
+                                      'y', 'z', '2', '3', '4', '5', '6', '7'];
+
+    static KEY_SIZE: usize = 7;
+
+    fn generate_key() -> String {
+        let mut rng = thread_rng();
+        (0..KEY_SIZE).map(|_| {
+            VALID_CHARS[rng.gen::<usize>() % 32]
+        }).collect()
+    }
+
+    impl CreateLink {
+        pub fn new(url: Url) -> CreateLink {
+            let host = url.host_str().unwrap_or("").to_owned();
+
             CreateLink {
-                url: url,
-                key: "",
-                domain: "",
-                title: "",
+                url: url.as_str().to_owned(),
+                key: generate_key(),
+                domain: host,
+                title: "".to_owned(),
                 is_custom: false
             }
         }
 
-        pub fn customize<'b>(&'b mut self, key: &'a str)
-            -> &'b mut CreateLink<'a> {
+        pub fn generate_key(&mut self) -> &mut CreateLink {
+            self.key = generate_key();
+            self
+        }
+
+        pub fn customize(&mut self, key: String) -> &mut CreateLink {
             self.key = key;
             self.is_custom = true;
             self
         }
 
-        pub fn set_title<'b>(&'b mut self, title: &'a str)
-            -> &'b mut CreateLink<'a> {
+        pub fn set_title(&mut self, title: String) -> &mut CreateLink {
             self.title = title;
             self
         }
