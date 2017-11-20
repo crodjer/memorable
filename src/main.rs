2e62c0ee85f7f2eefc21e6d1caad305d01adaf8a
Rohan Jain <crodjer@gmail.com>
2017-11-20 15:27:35 +0530

    Print help when no subcommand is present

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,14 @@ extern crate clap;
 extern crate memorable;
 
 use clap::{Arg, App, SubCommand};
-use self::memorable::db;
-use self::memorable::handlers;
+use memorable::db;
+use memorable::handlers;
+use std::process;
 
 // This seems to be designed to be glob imported.
 
 fn main() {
-    let matches = App::new(env!("CARGO_PKG_NAME"))
+    let mut app = App::new(env!("CARGO_PKG_NAME"))
         .version(env!("CARGO_PKG_VERSION"))
         .author(env!("CARGO_PKG_AUTHORS"))
         .about(env!("CARGO_PKG_DESCRIPTION"))
@@ -32,23 +33,24 @@ fn main() {
                     .about("Look up an already shortened link.")
                     .arg(Arg::with_name("short-link")
                          .required(true)
-                         .help("The shortend link to resolve.")))
-        .get_matches();
+                         .help("The shortend link to resolve.")));
+    let matches = app.clone().get_matches();
 
     // Clap gave us matches, connect to the DB.
     let connection = db::establish_connection();
 
     if let Some(matches) = matches.subcommand_matches("shorten") {
-        let link = handlers::links::create_link(
-            &connection,
-            matches.value_of("link").unwrap(),
-            matches.value_of("custom-key"),
-            matches.value_of("title")
-        );
+        let link = handlers::links::create_link(&connection,
+                                                matches.value_of("link").unwrap(),
+                                                matches.value_of("custom-key"),
+                                                matches.value_of("title"));
         println!("Link: {:?}", link.unwrap());
     } else if let Some(matches) = matches.subcommand_matches("lookup") {
         let short_link = matches.value_of("short-link").unwrap();
         println!("Link: {:?}", handlers::links::get_link(&connection,
                                                          short_link));
+    } else {
+        app.print_help().unwrap();
+        process::exit(1);
     }
 }
