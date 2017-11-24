5c1e4b834eba33221d5101e18c7a105fc1e0d082
Rohan Jain <crodjer@gmail.com>
2017-11-24 15:55:26 +0530

    Add PONG and fix route ordering

--- src/server.rs
+++ src/server.rs
@@ -7,6 +7,11 @@ use router::Router;
 use params::{Params, FromValue, Value};
 use middleware::diesel::DieselReqExt;
 
+/// Respond with a PONG when called. To be used as a health check.
+fn pong (_: &mut Request) -> IronResult<Response> {
+    Ok(Response::with((status::Ok, "PONG")))
+}
+
 fn get_link (req: &mut Request) -> IronResult<Response> {
     if let Some(key) = req.extensions.get::<Router>().unwrap().find("key") {
         let conn = req.db_conn();
@@ -36,7 +41,7 @@ fn create_link (req: &mut Request) -> IronResult<Response> {
             match links::create_link(&conn, url.to_owned(), title, custom_key) {
                 Ok(link) => Ok(Response::with((status::Ok, link.key))),
                 // This right now captures our errors as well.
-                Err(e) => Ok(Response::with((status::BadRequest,
+                Err(_) => Ok(Response::with((status::BadRequest,
                                              "URL is invalid.")))
             }
         },
@@ -48,8 +53,9 @@ fn create_link (req: &mut Request) -> IronResult<Response> {
 pub fn run () {
     let mut router = Router::new();
 
+    router.get("/ping", pong, "pong");
+    router.post("/shorten", create_link, "create-link");
     router.get("/:key", get_link, "get-link");
-    router.post("/", create_link, "create-link");
 
     let mut chain = Chain::new(router);
     chain.link_before(db::diesel_middleware());
